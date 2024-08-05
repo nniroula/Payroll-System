24a8f3f861726270f79cb3c0c937613b17db40ea
nabin <nkbhaie2017@gmail.com>
2024-08-05 15:05:12 -0600

    update Dao class

---
+++ src/main/java/epsV6/EmployeeModel.java
@@ -0,0 +1,75 @@
+package epsV6;
+
+/* Java is Object Oriented Programming language. So, create a class to use it as an Object */
+public class EmployeeModel {
+	private int employeeId;
+	private String employeeName;
+	private int employeeHoursWorked;
+	private double employeeHourlyWage;
+	private double employeeSalary;
+	private String employeeStatus;
+	
+	//getters
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	
+	public int getEmployeeHoursWorked() {
+		return employeeHoursWorked;
+	}
+	
+	public double getEmployeeHourlyWage() {
+		return employeeHourlyWage;
+	}
+	
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	
+	public String getEmployeeStatus() {
+		return employeeStatus;
+	}
+	
+	//setters
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	public void setEmployeeHoursWorked(int employeeHoursWorked) {
+		this.employeeHoursWorked = employeeHoursWorked;
+	}
+	
+	public void setEmployeeHourlyWage(double employeeHourlyWage) {
+		this.employeeHourlyWage = employeeHourlyWage;
+	}
+	
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	
+	public void setEmployeeStatus(String employeeStatus) {
+		this.employeeStatus = employeeStatus;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "DataModel [employeeName=" + employeeName + ", employeeId=" + employeeId + ", employeeHoursWorked="
+				+ employeeHoursWorked + ", employeeHourlyWage=" + employeeHourlyWage + ", employeeSalary="
+				+ employeeSalary + ", employeeStatus=" + employeeStatus + "]";
+	}
+	
+	public void print() {
+		System.out.println("IDDDDDDD in Model....");
+		System.out.println(employeeId);
+	}
+
+}
---
+++ src/main/java/epsV6/JdbcCredential.java
@@ -0,0 +1,26 @@
+package epsV6;
+
+/* final class cannot be extended or inherited 
+ * final variable -> to create constant variable
+ * final method -> to prevent method overriding
+ * final class -> to prevent inheritance
+ * */
+
+final public class JdbcCredential {
+	private String jdbcUrl = "jdbc:mysql://localhost:3306/eps_db";
+	private String username = "root"; // Replace root with your mysql username
+	String password = "root"; // Replace root with your mySQl password
+	
+	final public String getJdbcUrl() {
+		return jdbcUrl;
+	}
+	
+	final public String getUsername() {
+		return username;
+	}
+
+	final public String getPassword() {
+		return password;
+	}
+
+}
