7b817c5ac19ae05320bac55a50c1b617eb78b8a9
nabin <nkbhaie2017@gmail.com>
2024-09-18 10:31:29 -0600

    update package name

---
+++ src/main/java/eps/v2_io/Employee.java
@@ -0,0 +1,30 @@
+package eps.v2_io;
+
+abstract class Employee {
+	private String name;
+	private int id;
+	
+	Employee(String name, int id){
+		this.name = name;
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	// abstract methods
+	abstract double calculateSalary();
+	abstract String displaySalary();
+
+	@Override
+	public String toString() {
+		return "Employee - [name=" + name + ", id=" + id + "]";
+	}
+	
+	
+}
