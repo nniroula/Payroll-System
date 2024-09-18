364f98332f601feb48f5f631c079eda873a964d5
nabin <nkbhaie2017@gmail.com>
2024-09-18 11:12:49 -0600

    remove actual mysql credentials

--- src/main/java/eps/v7_jdbcConnector/JdbcConnectionCredentials.java
+++ src/main/java/eps/v7_jdbcConnector/JdbcConnectionCredentials.java
@@ -16,7 +16,7 @@ final public class JdbcConnectionCredentials {
 	private static String username = "root"; // Replace root with your mysql username
 	
 	/* Replace root with your mySQl password */
-	private static String password = "root"; 
+	private static String password = "root";
 
 	
 	/* best approach */
